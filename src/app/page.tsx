2fc515ca2a0da7978ec3de2fa2b3eaf160887092
Gilang Firza Rivanka <gifiri13@gmail.com>
2023-11-03 06:37:33 +0700

    perbaikan gagal deploy ke vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import "react-datepicker/dist/react-datepicker.css";
 import { PiExportBold } from "react-icons/pi";
 import { AiOutlineCaretDown } from "react-icons/ai";
 import jsPDF from "jspdf";
-import "jspdf-autotable";
 
 declare module "jspdf" {
   interface jsPDF {
